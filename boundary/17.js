109677bd964cf69bec1b5875f7c76b1b2ab822ad
Seung-wan <tmddhks0104@gmail.com>
2022-10-29 18:18:34 +0900

    17. first - last

---
+++ boundary/17.js
@@ -0,0 +1,12 @@
+// 17. first - last
+
+// 포함된 양 끝을 의미한다.
+// 부터 ~~~ 까지
+
+const students = ['poco', 'john', 'yu'];
+
+function getStudents(first, last) {
+  // ... some code
+}
+
+getStudents('poco', 'yu');
