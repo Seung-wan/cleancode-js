5c3a71a44a00f7a3f283f4b5940b22f6f8808c91
Seung-wan <tmddhks0104@gmail.com>
2022-10-29 18:19:07 +0900

    18. prefix - suffix

---
+++ boundary/18.js
@@ -0,0 +1,18 @@
+// 18. prefix - suffix
+
+// JavaScript의 getter, setter
+// React의 use prefix, hook이라는 의미
+// react-use, jQuery, lodash, underscore
+
+// JavaScript 생성자 함수의 내부 변수를 this._name = name, 암묵적으로 내부 변수를 나타내는 방법, 클래스 이전
+
+class User {
+  #name;
+}
+
+// Redux Action, suffix
+export const STARRED_REQUEST = 'STARRED_REQUEST';
+export const STARRED_SUCCESS = 'STARRED_SUCCESS';
+export const STARRED_FAILURE = 'STARRED_FAILURE';
+
+// 개인과 팀에서 네이밍, 규칙을 정할 때 prefix와 suffix를 붙이면 좋다. 일관성.
